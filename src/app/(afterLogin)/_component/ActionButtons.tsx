5af0265b613cc92d664af771b43d150ba6b6dc48
inkblot626 <inkblot626@gmail.com>
2024-01-04 22:50:53 +0900

    add actionbuttons

---
+++ src/app/(afterLogin)/_component/ActionButtons.tsx
@@ -0,0 +1,112 @@
+"use client";
+import cx from "classnames";
+
+export default function ActionButtons() {
+  const commented = true;
+  const reposted = true;
+  const liked = true;
+
+  const onClickComment = () => {};
+  const onClickRepost = () => {};
+  const onClickHeart = () => {};
+
+  const BUTTON_WRAPPER_CLASS = "group flex flex-row items-center";
+  const SVG_CLASS = "fill-[#536471]";
+  const BUTTON_CLASS =
+    "bg-white border-none outline-none flex w-10 h-10 items-center justify-center rounded-2xl cursor-pointer mr-1 transition-{background-color} duration-200";
+
+  return (
+    <div className="flex justify-between flex-row mt-3 gap-x-1 gap-y-1">
+      <div className={BUTTON_WRAPPER_CLASS}>
+        <button
+          onClick={onClickComment}
+          className={
+            BUTTON_CLASS + "bg-[#1d9bf0] bg-opacity-5 hover:bg-opacity-15"
+          }
+        >
+          <svg
+            width={24}
+            viewBox="0 0 24 24"
+            aria-hidden="true"
+            className={cx(
+              SVG_CLASS + " group-hover:fill-[#1d9bf0]",
+              commented && "fill-[#1d9bf0]"
+            )}
+          >
+            <g>
+              <path d="M1.751 10c0-4.42 3.584-8 8.005-8h4.366c4.49 0 8.129 3.64 8.129 8.13 0 2.96-1.607 5.68-4.196 7.11l-8.054 4.46v-3.69h-.067c-4.49.1-8.183-3.51-8.183-8.01zm8.005-6c-3.317 0-6.005 2.69-6.005 6 0 3.37 2.77 6.08 6.138 6.01l.351-.01h1.761v2.3l5.087-2.81c1.951-1.08 3.163-3.13 3.163-5.36 0-3.39-2.744-6.13-6.129-6.13H9.756z"></path>
+            </g>
+          </svg>
+        </button>
+        <div
+          className={cx(
+            "text-xs text-[#536471] group-hover:text-[#1d9bf0]",
+            commented && "text-[#1d9bf0]"
+          )}
+        >
+          {1 || ""}
+        </div>
+      </div>
+      <div className={BUTTON_WRAPPER_CLASS}>
+        <button
+          onClick={onClickRepost}
+          className={
+            BUTTON_CLASS + "bg-[#00ba7c] bg-opacity-5 hover:bg-opacity-15"
+          }
+        >
+          <svg
+            width={24}
+            viewBox="0 0 24 24"
+            aria-hidden="true"
+            className={cx(
+              SVG_CLASS + " group-hover:fill-[#00ba7c]",
+              reposted && "fill-[#00ba7c]"
+            )}
+          >
+            <g>
+              <path d="M4.5 3.88l4.432 4.14-1.364 1.46L5.5 7.55V16c0 1.1.896 2 2 2H13v2H7.5c-2.209 0-4-1.79-4-4V7.55L1.432 9.48.068 8.02 4.5 3.88zM16.5 6H11V4h5.5c2.209 0 4 1.79 4 4v8.45l2.068-1.93 1.364 1.46-4.432 4.14-4.432-4.14 1.364-1.46 2.068 1.93V8c0-1.1-.896-2-2-2z"></path>
+            </g>
+          </svg>
+        </button>
+        <div
+          className={cx(
+            "text-xs text-[#536471] group-hover:text-[#00ba7c]",
+            reposted && "text-[#00ba7c]"
+          )}
+        >
+          {1 || ""}
+        </div>
+      </div>
+      <div className={BUTTON_WRAPPER_CLASS}>
+        <button
+          onClick={onClickHeart}
+          className={
+            BUTTON_CLASS + "bg-[#f91880] bg-opacity-5 hover:bg-opacity-15"
+          }
+        >
+          <svg
+            width={24}
+            viewBox="0 0 24 24"
+            aria-hidden="true"
+            className={cx(
+              SVG_CLASS + " group-hover:fill-[#f91880]",
+              liked && "fill-[#f91880]"
+            )}
+          >
+            <g>
+              <path d="M16.697 5.5c-1.222-.06-2.679.51-3.89 2.16l-.805 1.09-.806-1.09C9.984 6.01 8.526 5.44 7.304 5.5c-1.243.07-2.349.78-2.91 1.91-.552 1.12-.633 2.78.479 4.82 1.074 1.97 3.257 4.27 7.129 6.61 3.87-2.34 6.052-4.64 7.126-6.61 1.111-2.04 1.03-3.7.477-4.82-.561-1.13-1.666-1.84-2.908-1.91zm4.187 7.69c-1.351 2.48-4.001 5.12-8.379 7.67l-.503.3-.504-.3c-4.379-2.55-7.029-5.19-8.382-7.67-1.36-2.5-1.41-4.86-.514-6.67.887-1.79 2.647-2.91 4.601-3.01 1.651-.09 3.368.56 4.798 2.01 1.429-1.45 3.146-2.1 4.796-2.01 1.954.1 3.714 1.22 4.601 3.01.896 1.81.846 4.17-.514 6.67z"></path>
+            </g>
+          </svg>
+        </button>
+        <div
+          className={cx(
+            "text-xs text-[#536471] group-hover:text-[#f91880]",
+            liked && "text-[#f91880]"
+          )}
+        >
+          {0 || ""}
+        </div>
+      </div>
+    </div>
+  );
+}
