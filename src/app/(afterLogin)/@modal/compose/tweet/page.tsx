4b40b22c819d795457d50c71629a29b59c774325
김규빈 <inkblot626@gmail.com>
2024-10-09 22:32:31 +0900

    css tailwind

--- src/app/(afterLogin)/@modal/compose/tweet/page.tsx
+++ src/app/(afterLogin)/@modal/compose/tweet/page.tsx
@@ -32,26 +32,26 @@ export default function TweetModal() {
             </g>
           </svg>
         </button>
-        <form className={style.modalForm} onSubmit={onSubmit}>
-          <div className={style.modalBody}>
-            <div className={style.postUserSection}>
-              <div className={style.postUserImage}>
+        <form className="flex flex-col flex-1" onSubmit={onSubmit}>
+          <div className="px-4 flex-1 mt-14 flex flex-row">
+            <div className="mr-3 w-10">
+              <div className="w-10 h-10 rounded-[20px]">
                 <img src={me.image} alt={me.id} />
               </div>
             </div>
-            <div className={style.inputDiv}>
+            <div className="flex-1">
               <textarea
-                className={style.input}
+                className="w-full border-none outline-none text-xl"
                 placeholder="무슨 일이 일어나고 있나요?"
                 value={content}
                 onChange={onChangeContent}
               />
             </div>
           </div>
-          <div className={style.modalFooter}>
-            <div className={style.modalDivider} />
-            <div className={style.footerButtons}>
-              <div className={style.footerButtonLeft}>
+          <div className="py-0 px-4">
+            <div className="w-full border-b border-gray-200" />
+            <div className="flex flex-row items-center">
+              <div className="flex-1">
                 <input
                   type="file"
                   name="imageFiles"
@@ -60,18 +60,23 @@ export default function TweetModal() {
                   ref={imageRef}
                 />
                 <button
-                  className={style.uploadButton}
+                  className="w-[34px] h-[34px] border-none cursor-pointer rounded-2xl bg-[#1d9bf0] bg-opacity-5 transition-{background-color} duration-200 flex justify-center items-center hover:bg-opacity-15"
                   type="button"
                   onClick={onClickButton}
                 >
-                  <svg width={24} viewBox="0 0 24 24" aria-hidden="true">
+                  <svg
+                    width={24}
+                    viewBox="0 0 24 24"
+                    aria-hidden="true"
+                    className="fill-[#1d9bf0]"
+                  >
                     <g>
                       <path d="M3 5.5C3 4.119 4.119 3 5.5 3h13C19.881 3 21 4.119 21 5.5v13c0 1.381-1.119 2.5-2.5 2.5h-13C4.119 21 3 19.881 3 18.5v-13zM5.5 5c-.276 0-.5.224-.5.5v9.086l3-3 3 3 5-5 3 3V5.5c0-.276-.224-.5-.5-.5h-13zM19 15.414l-3-3-5 5-3-3-3 3V18.5c0 .276.224.5.5.5h13c.276 0 .5-.224.5-.5v-3.086zM9.75 7C8.784 7 8 7.784 8 8.75s.784 1.75 1.75 1.75 1.75-.784 1.75-1.75S10.716 7 9.75 7z"></path>
                     </g>
                   </svg>
                 </button>
               </div>
-              <button className={style.actionButton} disabled={!content}>
+              <button className="actionButton" disabled={!content}>
                 게시하기
               </button>
             </div>
